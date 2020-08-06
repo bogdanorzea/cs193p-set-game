05e6b54b128f0e2a54fb30f9f5978552275a93db
Bogdan Orzea <bogdan.orzea@gmail.com>
2020-08-05 22:40:53 -0400

    Initial Commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Bogdan Orzea on 8/5/20.
+//  Copyright © 2020 Bogdan Orzea. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
